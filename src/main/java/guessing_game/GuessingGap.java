ab855576fe5d8ff0524ddccdcc39f1c795928d8f
Anthony Mkumbae <t_mauzo@yahoo.com>
2020-01-04 14:24:18 -0500

    initial commit

---
+++ src/main/java/guessing_game/GuessingGap.java
@@ -0,0 +1,25 @@
+package guessing_game;
+
+import java.util.Scanner;
+
+public class GuessingGap {
+
+	public static void main(String[] args) {
+		
+Scanner input = new Scanner(System.in);
+System.out.println ("Guess a number between 1 & 10:");
+int userGuess = input.nextln();
+if(userGuess == 7) {
+	System.out.println("You win");
+}else {
+	System.out.println("You lose!?");
+	
+	
+	
+}
+
+
+
+	}
+
+}
